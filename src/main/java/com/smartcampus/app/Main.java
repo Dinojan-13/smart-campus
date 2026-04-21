a24916f73c753d84d680cfd9ad2f7720d8928960
Dinojan-13 <dinojankt13@gmail.com>
2026-04-21 13:57:44 +0530

    fix errors

--- src/main/java/com/smartcampus/app/Main.java
+++ src/main/java/com/smartcampus/app/Main.java
@@ -3,16 +3,19 @@ package com.smartcampus.app;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-
+import org.glassfish.jersey.jackson.JacksonFeature;
 import java.net.URI;
 
 public class Main {
-    public static final String BASE_URI = "http://localhost:8080/";
+    public static final String BASE_URI = "http://localhost:8080/api/v1/";
 
     public static void main(String[] args) throws Exception {
-        ResourceConfig config = new ResourceConfig().packages("com.smartcampus");
+        ResourceConfig config = new ResourceConfig()
+                .packages("com.smartcampus")
+                .register(JacksonFeature.class);
+
         HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
-        System.out.println("Smart Campus API running at " + BASE_URI + "api/v1");
+        System.out.println("Smart Campus API running at " + BASE_URI);
         System.out.println("Press ENTER to stop...");
         System.in.read();
         server.stop();
--- src/main/java/com/smartcampus/app/SmartCampusApp.java
+++ src/main/java/com/smartcampus/app/SmartCampusApp.java
@@ -1,8 +1,33 @@
 package com.smartcampus.app;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import com.smartcampus.resource.RoomResource;
+import com.smartcampus.resource.SensorResource;
+import com.smartcampus.resource.DiscoveryResource;
+import com.smartcampus.exception.GlobalExceptionMapper;
+import com.smartcampus.exception.RoomNotEmptyExceptionMapper;
+import com.smartcampus.exception.SensorUnavailableExceptionMapper;
+import com.smartcampus.exception.LinkedResourceNotFoundExceptionMapper;
+import com.smartcampus.filter.LoggingFilter;
+
 @ApplicationPath("/api/v1")
 public class SmartCampusApp extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(RoomResource.class);
+        classes.add(SensorResource.class);
+        classes.add(DiscoveryResource.class);
+        classes.add(GlobalExceptionMapper.class);
+        classes.add(RoomNotEmptyExceptionMapper.class);
+        classes.add(SensorUnavailableExceptionMapper.class);
+        classes.add(LinkedResourceNotFoundExceptionMapper.class);
+        classes.add(LoggingFilter.class);
+        return classes;
+    }
 }
